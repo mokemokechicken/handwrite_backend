008189e28fd4f6b988b1746a635990b31d6a3c52
Ken Morishita <mokemokechicken@gmail.com>
2012-11-26 12:59:55 +0900

    init

---
+++ src/prjlib/learning.py
@@ -0,0 +1,138 @@
+# coding: utf8
+'''
+Created on 2012/11/20
+
+@author: k_morishita
+'''
+
+import os
+import sys
+import time
+import numpy
+
+import PIL
+
+import theano
+import theano.tensor as T
+
+def pretraining_model(dbn, train_set_x, pretraining_epochs, batch_size=10, pretrain_lr=0.01, k=1):
+    print '... getting the pretraining functions'
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0] / batch_size
+    pretraining_fns = dbn.pretraining_functions(train_set_x=train_set_x,
+                                                batch_size=batch_size,
+                                                k=k)
+
+    print '... pre-training the model'
+    start_time = time.clock()
+    ## Pre-train layer-wise
+    for i in xrange(dbn.n_layers):
+        # go through pretraining epochs
+        for epoch in xrange(pretraining_epochs):
+            # go through the training set
+            c = []
+            for batch_index in xrange(n_train_batches):
+                c.append(pretraining_fns[i](index=batch_index,
+                                            lr=pretrain_lr))
+            print 'Pre-training layer %i, epoch %d, cost ' % (i, epoch),
+            print numpy.mean(c)
+    
+    end_time = time.clock()
+    print >> sys.stderr, ('The pretraining code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((end_time - start_time) / 60.))
+    
+
+def evaluate_model(dbn, datasets):
+    train_fn, validate_model, test_model = dbn.build_finetune_functions(
+                datasets=datasets, batch_size=10,
+                learning_rate=0)
+    validation_losses = validate_model()
+    this_validation_loss = numpy.mean(validation_losses)
+    this_test_loss = numpy.mean(test_model())
+    print('validation error %f %%, test error %f %%' % (this_validation_loss * 100., this_test_loss * 100.))
+
+
+def fine_tune_model(dbn, datasets, training_epochs, batch_size, finetune_lr=0.1, improvement_threshold=0.995):
+    """
+    
+    param: improvement_threshold
+            a relative improvement of this much is considered significant
+    """
+    ########################
+    # FINETUNING THE MODEL #
+    ########################
+    n_train_batches = datasets[0][0].get_value(borrow=True).shape[0] / batch_size
+
+    # get the training, validation and testing function for the model
+    print '... getting the finetuning functions'
+    train_fn, validate_model, test_model = dbn.build_finetune_functions(
+                datasets=datasets, batch_size=batch_size,
+                learning_rate=finetune_lr)
+
+    print '... finetunning the model'
+    # early-stopping parameters
+    patience = 4 * n_train_batches  # look as this many examples regardless
+    patience_increase = 2.    # wait this much longer when a new best is
+                              # found
+    validation_frequency = min(n_train_batches, patience / 2)
+                                  # go through this many
+                                  # minibatche before checking the network
+                                  # on the validation set; in this case we
+                                  # check every epoch
+
+    best_params = None
+    best_validation_loss = numpy.inf
+    test_score = 1.
+    start_time = time.clock()
+
+    done_looping = False
+    epoch = 0
+
+    while (epoch < training_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in xrange(n_train_batches):
+
+            minibatch_avg_cost = train_fn(minibatch_index)
+            iter = epoch * n_train_batches + minibatch_index
+
+            if (iter + 1) % validation_frequency == 0:
+
+                validation_losses = validate_model()
+                this_validation_loss = numpy.mean(validation_losses)
+                print('epoch %i, minibatch %i/%i, validation error %f %%' % \
+                      (epoch, minibatch_index + 1, n_train_batches,
+                       this_validation_loss * 100.))
+
+                # if we got the best validation score until now
+                if this_validation_loss < best_validation_loss:
+
+                    #improve patience if loss improvement is good enough
+                    if (this_validation_loss < best_validation_loss *
+                        improvement_threshold):
+                        patience = max(patience, iter * patience_increase)
+
+                    # save best validation score and iteration number
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+                    # test it on the test set
+                    test_losses = test_model()
+                    test_score = numpy.mean(test_losses)
+                    print(('     epoch %i, minibatch %i/%i, test error of '
+                           'best model %f %%') %
+                          (epoch, minibatch_index + 1, n_train_batches,
+                           test_score * 100.))
+
+            if patience <= iter:
+                done_looping = True
+                break
+
+    end_time = time.clock()
+    print(('Optimization complete with best validation score of %f %%,'
+           'with test performance %f %%') %
+                 (best_validation_loss * 100., test_score * 100.))
+    print >> sys.stderr, ('The fine tuning code for file ' +
+                          os.path.split(__file__)[1] +
+                          ' ran for %.2fm' % ((end_time - start_time)
+                                              / 60.))
+
---
+++ src/prjlib/state_persistent.py
@@ -0,0 +1,35 @@
+# coding: utf8
+'''
+Created on 2012/11/16
+
+@author: k_morishita
+'''
+
+
+from cPickle import load, dump, HIGHEST_PROTOCOL
+import os
+import gzip
+
+
+def load_params(filename):
+    data = None
+    if os.path.exists(filename):
+        fin = gzip.open(filename, "rb")
+        data = load(fin)
+        fin.close()
+    return data
+
+def save_params(filename, machine):
+    basedir = os.path.abspath(os.path.dirname(filename))
+    if not os.path.isdir(basedir):
+        os.makedirs(basedir)
+    fout = gzip.open(filename, "wb")
+    dump(machine.serialize(), fout, protocol=HIGHEST_PROTOCOL)
+    fout.close()
+
+class StatePersistent(object):
+    def deserialize(self, params, num):
+        return [params.pop(0) for _ in range(num)]
+    
+    def serialize(self):
+        return self.params
